c3ae65a6261eb595508ee64914a258d70603942e
iamsaintAdrian <adriankimani5@gmail.com>
2024-05-03 00:44:15 +0300

    added buttons

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ function increment(){
     document.getElementById("number").textContent=count
 
 }
+
+function decrement(){
+    if(count > 0){
+        count -=1
+        document.getElementById("number").textContent=count
+    }
+}
+//defining the save function
 function save(){
     document.getElementById("entries").textContent += count + "-"
     count = 0
     document.getElementById("number").textContent=count
     
 }
+
+function reset(){
+    document.getElementById("entries").textContent ="Previous Entries: " 
+}
+
 
 
 
